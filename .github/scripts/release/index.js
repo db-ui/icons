c5c8218c754dd632bc0def1110e4fa56bdc7085c
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2024-12-10 16:44:29 +0100

    fix: issue with release id (#34)

--- .github/scripts/release/index.js
+++ .github/scripts/release/index.js
@@ -6,8 +6,6 @@ import uploadAsset from "./upload-asset.js";
 import zipFolder from "./zip-folder.js";
 
 const release = async ({ github, context, workspace, iconReleaseId }) => {
-  const { id: release_id } = context.payload.release;
-
   // 3. Upload latest icon assets
   const iconsName = "icons.zip";
   const iconsPath = `${workspace}/packages/core/dist/assets`;
